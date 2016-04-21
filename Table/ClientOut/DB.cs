4110fa9c2bd3b6f70b6465539cf364c8b8635109
xuke <825126369@qq.com>
2016-04-21 08:14:50 +0800

    测试配置表提交

---
+++ Table/ClientOut/DB.cs
@@ -0,0 +1,233 @@
+using System.Collections;
+using System.Collections.Generic;
+namespace xk_System.Db
+{
+	class Sheet1DB:DbBase
+	{
+		/// <summary>
+		/// 字段1
+		/// </summary>
+		public readonly int aaa;
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int ccc;
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly List<int> fff1;
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly List<int> fff2;
+		/// <summary>
+		/// 字段7
+		/// </summary>
+		public readonly List<int> fff3;
+		/// <summary>
+		/// 字段8
+		/// </summary>
+		public readonly List<int> fff4;
+		/// <summary>
+		/// 字段9
+		/// </summary>
+		public readonly List<int> fff5;
+		/// <summary>
+		/// 字段10
+		/// </summary>
+		public readonly List<int> fff6;
+		/// <summary>
+		/// 字段11
+		/// </summary>
+		public readonly List<int> fff7;
+		/// <summary>
+		/// 字段12
+		/// </summary>
+		public readonly List<int> fff8;
+		/// <summary>
+		/// 字段13
+		/// </summary>
+		public readonly List<int> fff9;
+		/// <summary>
+		/// 字段14
+		/// </summary>
+		public readonly List<int> fff10;
+		/// <summary>
+		/// 字段15
+		/// </summary>
+		public readonly List<int> fff11;
+		/// <summary>
+		/// 字段16
+		/// </summary>
+		public readonly List<int> fff12;
+		/// <summary>
+		/// 字段17
+		/// </summary>
+		public readonly List<int> fff13;
+		/// <summary>
+		/// 字段18
+		/// </summary>
+		public readonly List<int> fff14;
+		/// <summary>
+		/// 字段19
+		/// </summary>
+		public readonly List<int> fff15;
+	}
+
+	class Sheet2DB:DbBase
+	{
+		/// <summary>
+		/// 字段1
+		/// </summary>
+		public readonly int aaa;
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int ccc;
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly List<int> fff1;
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly List<int> fff2;
+		/// <summary>
+		/// 字段7
+		/// </summary>
+		public readonly List<int> fff3;
+		/// <summary>
+		/// 字段8
+		/// </summary>
+		public readonly List<int> fff4;
+		/// <summary>
+		/// 字段9
+		/// </summary>
+		public readonly List<int> fff5;
+		/// <summary>
+		/// 字段10
+		/// </summary>
+		public readonly List<int> fff6;
+		/// <summary>
+		/// 字段11
+		/// </summary>
+		public readonly List<int> fff7;
+		/// <summary>
+		/// 字段12
+		/// </summary>
+		public readonly List<int> fff8;
+		/// <summary>
+		/// 字段13
+		/// </summary>
+		public readonly List<int> fff9;
+		/// <summary>
+		/// 字段14
+		/// </summary>
+		public readonly List<int> fff10;
+		/// <summary>
+		/// 字段15
+		/// </summary>
+		public readonly List<int> fff11;
+		/// <summary>
+		/// 字段16
+		/// </summary>
+		public readonly List<int> fff12;
+		/// <summary>
+		/// 字段17
+		/// </summary>
+		public readonly List<int> fff13;
+		/// <summary>
+		/// 字段18
+		/// </summary>
+		public readonly List<int> fff14;
+		/// <summary>
+		/// 字段19
+		/// </summary>
+		public readonly List<int> fff15;
+	}
+
+	class Sheet3DB:DbBase
+	{
+		/// <summary>
+		/// 字段1
+		/// </summary>
+		public readonly int aaa;
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int ccc;
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly List<int> fff1;
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly List<int> fff2;
+		/// <summary>
+		/// 字段7
+		/// </summary>
+		public readonly List<int> fff3;
+		/// <summary>
+		/// 字段8
+		/// </summary>
+		public readonly List<int> fff4;
+		/// <summary>
+		/// 字段9
+		/// </summary>
+		public readonly List<int> fff5;
+		/// <summary>
+		/// 字段10
+		/// </summary>
+		public readonly List<int> fff6;
+		/// <summary>
+		/// 字段11
+		/// </summary>
+		public readonly List<int> fff7;
+		/// <summary>
+		/// 字段12
+		/// </summary>
+		public readonly List<int> fff8;
+		/// <summary>
+		/// 字段13
+		/// </summary>
+		public readonly List<int> fff9;
+		/// <summary>
+		/// 字段14
+		/// </summary>
+		public readonly List<int> fff10;
+		/// <summary>
+		/// 字段15
+		/// </summary>
+		public readonly List<int> fff11;
+		/// <summary>
+		/// 字段16
+		/// </summary>
+		public readonly List<int> fff12;
+		/// <summary>
+		/// 字段17
+		/// </summary>
+		public readonly List<int> fff13;
+		/// <summary>
+		/// 字段18
+		/// </summary>
+		public readonly List<int> fff14;
+		/// <summary>
+		/// 字段19
+		/// </summary>
+		public readonly List<int> fff15;
+	}
+
+}
