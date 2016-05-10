662dba6c7949ac42fd7ae73acaa4b9182c67e7ce
xuke <825126369@qq.com>
2016-05-10 12:26:49 +0800

    注册账号，登陆账号协议

--- Table/ClientOut/DB.cs
+++ Table/ClientOut/DB.cs
@@ -2,7 +2,175 @@ using System.Collections;
 using System.Collections.Generic;
 namespace xk_System.Db
 {
+	class Sheet5DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet6DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet7DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet8DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet1DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
 	class Sheet2DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet3DB:DbBase
+	{
+		/// <summary>
+		/// 字段2
+		/// </summary>
+		public readonly string bbb1;
+		/// <summary>
+		/// 字段3
+		/// </summary>
+		public readonly int[] bbb2=new int[4];
+		/// <summary>
+		/// 字段4
+		/// </summary>
+		public readonly string[] bbb3=new string[4];
+		/// <summary>
+		/// 字段5
+		/// </summary>
+		public readonly string[] bbb4=new string[6];
+		/// <summary>
+		/// 字段6
+		/// </summary>
+		public readonly int[] bbb5=new int[6];
+	}
+
+	class Sheet4DB:DbBase
 	{
 		/// <summary>
 		/// 字段2
